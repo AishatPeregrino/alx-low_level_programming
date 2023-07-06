ca8d923601930882bd1479fff819d4f898e979cb
Aishat Peregrino-Brimah <you@example.com>
2023-07-06 06:03:02 -0700

    100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
  * Endianness Protype AishatPeregrinoBrimah
  * If the least significant byte is stored at the lowest address,
  * it means the system is little endian.
  */
-
 int get_endianness(void)
 {
 	unsigned int x;
