eb90783ec8b976278049bfdaf1729bd99c57710d
Aishat Peregrino-Brimah <you@example.com>
2023-07-06 03:47:51 -0700

    100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * Author:Aishat Peregrino-Brimah
+ * Endianness Protype
+ */
+
+
+int get_endianness() {
+    unsigned int num = 1;
+    char* ptr = (char*)&num;
+
+    // If the least significant byte is stored at the lowest address,
+    // it means the system is little endian.
+    if (*ptr == 1)
+        return 1; // Little endian
+    else
+        return 0; // Big endian
+}
+
+int main() {
+    int endianness = get_endianness();
+    
+    if (endianness == 1)
+        printf("Little endian\n");
+    else
+        printf("Big endian\n");
+    
+    return 0;
+}
+
